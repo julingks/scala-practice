0fe559f4d571cc54ff455e86c5ef993a428c82d9
Minwoo Kim <minwoo.kim@devsisters.com>
2015-04-15 22:54:45 +0900

    fix example

--- examples/first_example.scala
+++ examples/first_example.scala
@@ -1,24 +1,25 @@
-
-def sort(xs: Array[Int]) {
-  def swap(i: Int, j :Int) {
-    val t = xs(i); xs(i) = xs(j); xs(j) = t
-  }
-
-  def sort1(l: Int, r:Int) {
-    val pivot = xs((l+r)/2)
-    var i = l; var j = r
-    while (i <= j) {
-      while (xs(i) < pivot) i += 1
-      while (xs(j) > pivot) j -= 1
-      if (i <= j) {
-        swap(i, j)
-        i += 1
-        j -= 1
-      }
+class Test {
+  def sort(xs: Array[Int]) {
+    def swap(i: Int, j :Int) {
+      val t = xs(i); xs(i) = xs(j); xs(j) = t
     }
 
-    if (l<j) sort1(l, j)
-    if (j<r) sort1(i, r)
+    def sort1(l: Int, r:Int) {
+      val pivot = xs((l+r)/2)
+      var i = l; var j = r
+      while (i <= j) {
+        while (xs(i) < pivot) i += 1
+        while (xs(j) > pivot) j -= 1
+        if (i <= j) {
+          swap(i, j)
+          i += 1
+          j -= 1
+        }
+      }
+
+      if (l<j) sort1(l, j)
+      if (j<r) sort1(i, r)
+    }
+    sort1(0, xs.length -1)
   }
-  sort1(0, xs.length -1)
 }
